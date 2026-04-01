fedef3fc964dd0ce582d8314f66f70c96be75ab3
akshithasangam1759-dotcom <akshitha.sangam1759@gmail.com>
2026-04-02 00:46:04 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({ 
-  origin: ['http://localhost:5173', 'https://hungry-platter.vercel.app/'],
+  origin: true,
   credentials: true 
 }));
 app.use(express.json());
